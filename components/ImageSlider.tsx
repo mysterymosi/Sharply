5213ffd65a2da74379fda91b7d6b323059e68741
bamiogunfemi <ayobamiarafat09@gmail.com>
2022-09-27 11:49:46 +0100

    update schoool names

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -2,9 +2,17 @@
 import Slider from "react-slick";
 import { imageSliderSettings, urlFor } from "../utils";
 
-export const ImageSlider = ({ images,id }: { images: any , id?:string}) => {
-
-  const schools =['Queens College, Yaba', 'St. Finbarr’s College, Akoka', 'Ladela Schools, Abuja', ' Baptist Boys’ Academy, Obanikoro', 'Igbobi College, Yaba', 'Reagan Memorial Baptist Girls, Yaba', 'Federal Science and Technical College, Yaba', 'Federal Science and Technical College, Yaba', 'Methodist Boys’ High School, Victoria Island']
+export const ImageSlider = ({ images, id }: { images: any; id?: string }) => {
+  const schools = [
+    "Queens College, Yaba",
+    "St. Finbarr’s College, Akoka",
+    "Ladela Schools, Abuja",
+    " Baptist Boys’ Academy, Obanikoro",
+    "Igbobi College, Yaba",
+    "Reagan Memorial Baptist Girls, Yaba",
+    "Federal Science and Technical College, Yaba",
+    "Methodist Boys’ High School, Victoria Island",
+  ];
   return (
     <Slider {...imageSliderSettings}>
       {images.map((img: string, i: number) => (
